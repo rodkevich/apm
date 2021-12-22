9274ee590055caa5e72afc9c841a003a10bdad05
404 <404@test.it>
2021-12-22 20:57:44 +0300

    add: start with conc

---
+++ example01/con01.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func one(a string) {
+	fmt.Println(a)
+	wg.Done()
+}
+
+func two(b string) {
+	fmt.Println(b)
+	wg.Done()
+}
+
+func main() {
+	wg.Add(2)
+	go one("string for one")
+	go two("string for two")
+	//time.Sleep(3 *time.Second)
+	wg.Wait()
+}
